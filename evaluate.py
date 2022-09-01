- Added image normalisation - Added PCA implementation - Added the implementation of the additional loss term - Log validation (fixed frequency of logging)

evaluate.py
@@ -129,22 +129,18 @@ def validate_sintel(model, iters=32):
     return results
 
 @torch.no_grad()
-def validate_acdc(model, args, epoch, iters=2):
+def validate_acdc(model, args, epoch, mode, iters=2):
     ''' Perform validation using ACDC processed dataset '''
     cuda_to_use = "cuda:" + str(args.gpus[0])
     model.eval()
     val_dataset = datasets.ACDCDataset(folder_path=args.dataset_folder, 
-                                       max_seq_len=args.max_seq_len, mode='validation')
+                                       max_seq_len=args.max_seq_len, mode=mode)
     out_list = []
     total_loss, total_error, total_spa_loss, total_temp_loss = 0, 0, 0, 0
     for val_id in range(len(val_dataset)):
         image_batch, template_batch = val_dataset[val_id]
         image_batch = image_batch[None].to(cuda_to_use)
         template_batch = template_batch[None].to(cuda_to_use)
-
-        #padder = InputPadder(image_batch.shape, mode='acdc')
-        #image_batch, template_batch = padder.pad(image_batch, template_batch)
-
         flow_predictions1, flow_predictions2 = model(image_batch, template_batch, iters=iters, test_mode=True) #[B, 2, H, W]
         loss, error, spa_loss, temp_loss = Losses.disimilarity_loss(image_batch, template_batch, 
                                            flow_predictions1, flow_predictions2, 
@@ -211,6 +207,15 @@ if __name__ == '__main__':
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
+    
+    parser.add_argument('--gpus', type=int, nargs='+', default=[0,1])
+    parser.add_argument('--gamma', type=float, default=0.8, help='exponential weighting')
+    parser.add_argument('--dataset_folder', type=str)
+    parser.add_argument('--max_seq_len', type=int, default=35)
+    parser.add_argument('--beta_photo', type=float, default=1.0)
+    parser.add_argument('--beta_spatial', type=float, default=0.0)
+    parser.add_argument('--beta_temporal', type=float, default=0.0)
+    
     args = parser.parse_args()
 
     model = torch.nn.DataParallel(RAFT(args))
@@ -231,5 +236,7 @@ if __name__ == '__main__':
 
         elif args.dataset == 'kitti':
             validate_kitti(model.module)
+        elif args.dataset == 'acdc':
+            validate_acdc(model.module, args, 1, mode='testing', iters=2)
 
 

losses.py
@@ -39,9 +39,8 @@ def disimilarity_loss(img_gt, temp_gt, flow_forward, flow_backward, epoch, mode,
         img_pred = seq_utils.warp_batch(temp_gt, flow_backward[itr], gpu=args.gpus[0]) # [B, N, H, W]
         
         Charbonnier_Loss = CharbonnierLoss()
-        photo_loss = Charbonnier_Loss(temp_pred, temp_gt) + Charbonnier_Loss(img_pred, img_gt) #[B, N] loss of batch in iteration i
-        
-        Spatial_Loss = SpatialSmooth(grad=1, boundary_awareness=False)
+        photo_loss = Charbonnier_Loss(temp_pred, temp_gt) + Charbonnier_Loss(img_pred, img_gt) #[B, N] loss batch in iteration i
+        Spatial_Loss = SpatialSmooth(grad=1, boundary_awareness=True)
         spatial_loss = Spatial_Loss(flow_forward[itr][0,:,:,:,:], img_gt) + Spatial_Loss(flow_backward[itr][0,:,:,:,:], temp_gt)
         partial_spatial_loss += spatial_loss
         
@@ -50,17 +49,10 @@ def disimilarity_loss(img_gt, temp_gt, flow_forward, flow_backward, epoch, mode,
         partial_temporal_loss += temporal_loss 
         
         partial_loss += (photo_loss * args.beta_photo + spatial_loss * args.beta_spatial + temporal_loss * args.beta_temporal) * args.gamma ** (total_iter - itr - 1)
-        '''
-        if (mode == "training"):
-            wandb.log({"Training Spatial Loss": spatial_loss})
-            wandb.log({"Training Temporal Loss": spatial_loss})
-        elif (mode == "validation"):
-            wandb.log({"Validation Spatial Loss": spatial_loss})
-            wandb.log({"Validation Temporal Loss": spatial_loss})
-        '''
+       
         l1_loss = torch.nn.L1Loss()
         partial_error += l1_loss(temp_pred, temp_gt) + l1_loss(img_pred, img_gt)
-        if (itr == total_iter-1 and (mode == "training" and i_batch % 700 == 0) or (mode == "validation" and i_batch % 200 == 0)):
+        if (itr == total_iter-1 and (mode == "training" and i_batch % 700 == 0) or (mode == "validation" and i_batch % 50 == 0)):
             flow_for = flow_vis.flow_to_color(flow_forward[itr][0,3,:,:,:].permute(1,2,0).cpu().detach().numpy())
             flow_back = flow_vis.flow_to_color(flow_backward[itr][0,3,:,:].permute(1,2,0).cpu().detach().numpy())
             
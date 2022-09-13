faaeca3794ade845dc1a057af9fd5076c8b946d1
ManalHamdi <manal.hamdi@yahoo.fr>
2022-09-13 15:17:18 +0200

    Modified the scripts

--- script_train_gpu_server.sh
+++ script_train_gpu_server.sh
@@ -10,5 +10,5 @@ module load python/anaconda3
 conda activate raft
 mkdir -p checkpoints
 CUDA_VISIBLE_DEVICES=7
-/home/guests/manal_hamdi/.conda/envs/raft/bin/python -u train_new.py --name raft-acdc --stage acdc --validation acdc --dataset_folder "/home/guests/manal_hamdi/manal/RAFT/datasets/ACDC_processed/" --num_steps 100 --gpus 7 --batch_size 1 --lr 0.0004 --wdecay 0.0001 --max_seq_len 19 --beta_photo 1.0 --beta_spatial 10.0 --beta_temporal 0.0 #--restore_ckpt "checkpoints/1_raft-acdc.pth"
+/home/guests/manal_hamdi/.conda/envs/raft/bin/python -u train.py --name raft-acdc --stage acdc --validation acdc --dataset_folder "/home/guests/manal_hamdi/manal/RAFT/datasets/ACDC_processed/" --num_steps 100 --gpus 7 --batch_size 1 --lr 0.0004 --wdecay 0.0001 --max_seq_len 19 --beta_photo 1.0 --beta_spatial 10.0 --beta_temporal 0.0 #--restore_ckpt "checkpoints/1_raft-acdc.pth"
  

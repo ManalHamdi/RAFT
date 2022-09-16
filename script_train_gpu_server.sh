f20cb0fcb74e608c510931ae85416876761be9a7
Manal Hamdi <manal_hamdi@c1-node01.aim.in.tum.de>
2022-09-16 10:27:00 +0200

    script

--- script_train_gpu_server.sh
+++ script_train_gpu_server.sh
@@ -1,14 +1,17 @@
+#!/bin/sh
 #SBATCH -J "RAFT"   # job name
 #SBATCH --mail-user=manal.hamdi@tum.de   # email address
 #SBATCH --mail-type=END  # send at the end
 #SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1  # gpus if needed
+#SBATCH --output=/home/guests/manal_hamdi/manal/RAFT/output.out  # Standard output of the script (Can be absolute or relative path)
+#SBATCH --error=/home/guests/manal_hamdi/manal/RAFT/error.err  # Standard error of the script
 # run your program here
 
 module load python/anaconda3
 conda activate raft
 mkdir -p checkpoints
-CUDA_VISIBLE_DEVICES=7
-/home/guests/manal_hamdi/.conda/envs/raft/bin/python -u train.py --name raft-acdc --stage acdc --validation acdc --dataset_folder "/home/guests/manal_hamdi/manal/RAFT/datasets/ACDC_processed/" --num_steps 100 --gpus 7 --batch_size 1 --lr 0.0004 --wdecay 0.0001 --max_seq_len 20 --beta_photo 1.0 --beta_spatial 10.0 --beta_temporal 0.0 #--restore_ckpt "checkpoints/1_raft-acdc.pth"
+CUDA_VISIBLE_DEVICES=0
+/home/guests/manal_hamdi/.conda/envs/raft/bin/python -u train.py --name raft-acdc --stage acdc --validation acdc --dataset_folder "/home/guests/manal_hamdi/manal/RAFT/datasets/ACDC_processed/" --num_steps 100 --gpus 0 --batch_size 1 --lr 0.0004 --wdecay 0.0001 --max_seq_len 10 --beta_photo 1.0 --beta_spatial 10.0 --beta_temporal 0.0 #--restore_ckpt "checkpoints/1_raft-acdc.pth"
  

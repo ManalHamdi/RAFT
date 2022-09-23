bc7d5baeb982e9e86c6db4cd9ad2488b90285972
ManalHamdi <manal.hamdi@yahoo.fr>
2022-09-23 12:14:12 +0200

    script

--- script_train_kevin.sh
+++ script_train_kevin.sh
@@ -12,4 +12,4 @@ conda deactivate
 conda activate raftkevin
 echo "Activated raftkevin"
 mkdir -p checkpoints
-python3 -u train.py --name raft-acdc --stage acdc --validation acdc --dataset_folder "/home/kevin/manal/RAFT/datasets/ACDC_processed/" --num_steps 300 --gpus 1 --batch_size 1 --lr 0.0000004 --wdecay 0.0001 --max_seq_len 19 --add_normalisation --gamma 0.8 --beta_photo 1.0 --beta_spatial 10.0 --beta_temporal 0.0 --restore_ckpt "new_checkpoints/raft-acdc.pth"
+python3 -u train.py --name noNorm_1photo10spa0temp --stage acdc --validation acdc --dataset_folder "/home/kevin/manal/RAFT/datasets/ACDC_processed/" --num_steps 300 --gpus 1 --batch_size 1 --lr 0.0004 --wdecay 0.0001 --max_seq_len 19 --gamma 0.8 --beta_photo 1.0 --beta_spatial 10.0 --beta_temporal 0.00 --restore_ckpt "new_checkpoints/raft-acdc.pth"
